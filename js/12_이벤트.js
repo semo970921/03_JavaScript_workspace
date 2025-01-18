314cb85d4f5528d7baf8edf079c80f067829bec6
SeungWon <semo970921@naver.com>
2025-01-18 14:35:00 +0900

    20250118_이벤트 문제 추가

--- js/12_이벤트.js
+++ js/12_이벤트.js
@@ -1,24 +1,24 @@
 /** 인라인 이벤트 모델 확인 + this */
 /* 다음 JS 함수는 클릭 이벤트에 따라 div 요소의 내용을 변경하는 기능 수행 */
 
-function check1(div){
-    console.log(div);
+function check1(divs){
+    console.log(divs);
 
     /* 클릭이 되면 클릭된 숫자를 버튼(this를 통해 전달된 div)에 출력 */
 
     //  1)버튼에 작성된 내용 얻어오기
-    let num = Number(div.innerText)
+    let num = Number(divs.innerText)
     // -> 숫자 또는 NaN
 
     // 2) 얻어온 내용이 숫자가 아닌 경우
     // isNaN(값) : 값이 NaN인 경우 true, 숫자면 false
     if(isNaN(num)){ // 숫자가 아닌 경우
-        div.innerText = 1;
+        divs.innerText = 1;
         return;
     }
 
     // 3) 얻어온 내용이 숫자인 경우
-    div.innerText = ++num; 
+    divs.innerText = ++num; 
     
 };
 
@@ -53,52 +53,53 @@ testC.onclick = function(){
 
 
 //--------------------------------------------------------
-/* 클릭할 수록 투명 + 클릭 횟수 증가 */
+/* 클릭할 수록 투명 */
 const test1 = document.querySelector("#test1");
 
-// 클릭할 수록 투명(첫 번째 감지기)
 test1.addEventListener("click", function(){
-    // click : 감지할 이벤트 종류
-    // function(){} : 이벤트가 감지되었을 때 수행할 이벤트 핸들러
-
     let current = test1.style.opacity;
 
-    console.log("현재 투명도 : ", current);
-    // => 처음엔 ""(빈칸)
-    
-   if(current === ""){
-    current = 1;
-   }
+    // 현재의 불투명 정도 확인
+    // 처음엔 ""(빈칸)
+    console.log("current: ", current); 
 
-   test1.style.opacity = current - 0.05; // 투명도를 0.05ㄱ감소
+    if(current === ""){
+        current = 1;
+    };
 
-   if(test1.style.opacity < 0){ // 투명도가 0 미만이 된 경우(완전히 투명해진 이후)
-        test1.style.opacity = 1; 
-   }
+    test1.style.opacity = current - 0.05; // 투명도를 0.05 감소
+
+    if(test1.style.opacity < 0){
+        test1.style.opacity = 1;
+    };
 });
 
-// 클릭 시 숫자 1씩 증가(두 번째 감지기)
+/*  + 클릭 횟수 증가 */
 test1.addEventListener("click", function(){
-    
-    // #test1에 작성된 내용 얻어와 숫자로 변경
     let num = Number(test1.innerText);
     test1.innerText = ++num;
 });
 
-/* 클릭한 버튼에 작성된 색으로 배경색 변경하기  */
+/* 버튼 배경색 변경 + 클릭시 #result2 배경색 변경 */
 const btns = document.querySelectorAll(".btn-container > button");
-// btns는 유사배열 형태!
 const result2 = document.querySelector("#result2");
 
 for(let i=0; i<btns.length; i++){
-
-    // 각 버튼의 배경색을 버튼에 작성된 색상으로 변경
     btns[i].style.backgroundColor = btns[i].innerText;
 
-    // 각 버튼에 클릭 되었을 때(이벤트 리스너)의 동작(이벤트 핸들러)을 추가
-    btns[i].addEventListener("click",function(){
-
-        // #result2의 배경색을 클릭된 버튼에 작성된 색상으로 변경
+    // 각 버튼에 클릭 되었을 때(이벤트 리스너)의 동작(이벤트 핸들러) 추가
+    btns[i].addEventListener("click", function(){
         result2.style.backgroundColor = btns[i].innerText;
-    })
-}
+    });
+};
+
+/* 버튼 클릭시 input 요소에 작성된 색상 값 얻어와 배경색 변경하기 */
+const changeBtn = document.querySelector("#changeBtn");
+const inputColor = document.querySelectorAll(".inputColor");
+const display = document.querySelectorAll(".display");
+
+changeBtn.addEventListener("click", function(){
+    for(let i=0; i<inputColor.length; i++){
+        display[i].style.backgroundColor = inputColor[i].value; 
+    };
+});
